24ac92fc41a804bca1f60fa47d5ed965ad8b9039
Adlan Hawari <hwr.adlan@gmail.com>
2022-03-29 00:44:19 +0700

    w

--- utils/handleFormData.js
+++
@@ -1,9 +0,0 @@
-export default function handleFormData(objects){
-    let formData = new FormData();
-      for (let item in objects){
-        formData.append(item, objects[item])
-      }
-      
-
-      return formData
-}
