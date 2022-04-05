8b38713300d6f15b58c8fa53dde4abaf1db02ae7
Adlan Hawari <hwr.adlan@gmail.com>
2022-04-06 06:05:49 +0700

    manajemen user button

--- pages/manajemen/manajemen-user/index.js
+++ pages/manajemen/manajemen-user/index.js
@@ -1,3 +1,5 @@
+import { UsersIcon } from '@heroicons/react/outline';
+import { PlusIcon } from '@heroicons/react/solid';
 import BaseLayout from 'components/base/BaseLayout';
 import DateFilter from 'components/base/filter/DateFilter';
 import UserTable from 'components/base/table/UserTable';
@@ -17,8 +19,27 @@ export default function ManajemenManajemenUserPage() {
   return(
     <div className="block space-y-7">
 
-      <div className="flex">
+      <div className="flex justify-between items-center">
+        <div className="">
           <DateFilter/>
+        </div>
+
+        <div className="flex space-x-4">
+           <button className="flex items-center space-x-2 bg-primary button-base text-white px-5 py-2 rounded-xl"
+            >
+              <UsersIcon className="h-4 w-4 cursor-pointer" aria-hidden="true"/>
+              <p>
+                Roles dan Permission User
+              </p>
+          </button>
+          <button className="flex items-center space-x-2 bg-primary button-base text-white px-5 py-2 rounded-xl"
+            >
+              <PlusIcon className="h-6 w-6 cursor-pointer" aria-hidden="true"/>
+              <p>
+                Tambah User Baru
+              </p>
+          </button>
+        </div>
       </div>
       <UserTable data={users}/>
   </div>
