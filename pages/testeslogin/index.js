bf8cc25932c44e4902ccca6ba1fc6fe1e93fc5b0
Adlan Hawari <hwr.adlan@gmail.com>
2022-08-24 01:42:40 +0700

    z index header

--- pages/testeslogin/index.js
+++ pages/testeslogin/index.js
@@ -1,46 +1,30 @@
+import JenisPekerjaanFilter from 'components/base/filter/JenisPekerjaanFilter';
+import ProfileDropdown from 'components/small/single_menu/ProfileDropdown';
+import JenisPekerjaanFilterContextProvider from 'hooks/context/filter-jenisPekerjaan/JenisPekerjaanFilter';
+import TitleContextProvider from 'hooks/TitleContext';
 import React, { useEffect } from 'react'
 import useSWR from 'swr';
 import GetToken from 'utils/GetToken';
 
 export default function TestesLoginPage() {
-    const token = GetToken()
-
-    // const formValues 
-    var requestOptions = {
-        method:'GET'  ,
-        headers: {
-            'Authorization': `Bearer ${token}`,
-            // 'Content-Type': 'application/json'
-        },
-        redirect: 'follow'
-        };
-
-    const fetcher = async url => {
-        const res = await fetch(url, requestOptions)
-        if (!res.ok) {
-            const error = new Error('An error occurred while fetching the data.')
-            // Attach extra info to the error object.
-            error.info = await res.json()
-            error.status = res.status
-            throw error
-          }
-        
-          return res.json()
-    }
-    const api = "http://api.play1.musagreen.com/v1/users/profile"
-    const { data, error } = useSWR(api, fetcher)
-
-    useEffect(() => {
-      if(data){
-        console.log("data",data)
-      }
-      if(error){
-        console.log("err",error)
-      }
-    
-    }, [data, error])
-    
+   
   return (
-    <div>index</div>
+    <JenisPekerjaanFilterContextProvider>
+      <TitleContextProvider>
+
+
+    <div className='relative'>
+      <div className="absolute right-0">
+        <ProfileDropdown/>
+      </div>
+      <div className="absolute top-20 right-0">
+        
+        <JenisPekerjaanFilter/>
+      </div>
+      
+
+    </div>
+      </TitleContextProvider>
+    </JenisPekerjaanFilterContextProvider>
   )
 }
